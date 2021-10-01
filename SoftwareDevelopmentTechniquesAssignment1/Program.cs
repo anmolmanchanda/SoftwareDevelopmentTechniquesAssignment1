5153def9fc49ff8b52d8f60b6d634ffe9816ab4c
Anmol Manchanda <Anmolmanchanda5560@conestogac.on.ca>
2021-09-30 20:31:45 -0400

    Add project files.

---
+++ SoftwareDevelopmentTechniquesAssignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareDevelopmentTechniquesAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
